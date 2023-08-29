48c2a974c4115d693e8ee522af4e2cbe85374b9c
kaylacampbell1 <kaylacampbell3003@gmail.com>
2023-08-29 11:55:10 -0400

    added html bones to prepare for DOM manipulation

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 //js code
 
+const testRoute = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log("data:", data);
+};
+const url2 = "https://www.themealdb.com/api/json/v1/1/random.php";
+
+testRoute(url2);
