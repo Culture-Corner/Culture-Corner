a93cccdc793ce3116d08fa89bf3c306812c62941
nathanielrobinson24 <n.rbinson@aol.com>
2023-08-30 16:50:14 -0400

    solutions

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let introText = document.querySelector("#intro-text")
 let countriesDiv = document.querySelector("#countries")
 
+
 //show name of country and image
 const display = async () => {
   try {
@@ -29,46 +30,54 @@ let country = document.querySelectorAll(".country");
 
 async function showCousine(event) {
   try {
-    // if (event.srcElement/* === "div.class="country"*/) {
     if (event.target.classList[0] === "country") {
     let area = await event.target.id;
     let cousineResponse = await fetch(
       "https://www.themealdb.com/api/json/v1/1/filter.php?a=" + area
     );
     let cousineMeals = await cousineResponse.json()
-    console.log(cousineMeals)
     introText.textContent = `${area} Meals`
     countries.textContent = ""
     for (let idx = 0; idx < cousineMeals.meals.length; idx++) {
       let mealDetails = cousineMeals.meals[idx];
       let mealDiv = document.createElement("div")
       mealDiv.setAttribute("id", "meal-div")
+      mealDiv.dataset.id = mealDetails.idMeal;
       let foodName = document.createElement("h3");
       foodName.textContent = mealDetails.strMeal;
+      foodName.setAttribute("id", "food-name")
+      foodName.dataset.id = mealDetails.idMeal;
       let foodImg = document.createElement("img");
       foodImg.setAttribute("src", mealDetails.strMealThumb)
       foodImg.setAttribute("class", "cousine-meal-img");
+      foodImg.dataset.id = mealDetails.idMeal;
       countries.append(mealDiv)
       mealDiv.append(foodName)
       mealDiv.append(foodImg);
 
-      console.log(mealDetails)
     }
     }
-
-  
-  // } else {
-  //   console.log("not country");
-  // }
   } catch(error) {
     console.log("error: " + error)
   }
-  
-
-  
 }
 
-countries.addEventListener("click", showCousine);
 
-let list = document.querySelector("#joke-list")
+const showMeal = async (event) => {
+  try {
+    if (event.target.dataset.id) {
+    console.log("meal clicked")
+    let id = await event.target.dataset;
+    console.log(id)
+    
+    }
+  } catch(error) {
+    console.log("error: " + error)
+  }
+}
+
+
+
+countries.addEventListener("click", showCousine);
+countries.addEventListener("click", showMeal);
 
